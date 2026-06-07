6404f11faf49fabfcada71c94531b3d0997e7426
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-07 17:22:01 -0300

    Fundamentos do Next.js - Criando o Footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function Footer() {
+  return (
+    <footer className="border-t border-white/10">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex md:flex-row justify-between gap-8 py-8">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
+          </Link>
+
+          <nav className="flex flex-col md:flex-row items-center gap-16 text-sm text-muted-foreground">
+            <Link href="/termos-de-uso" className="hover:text-primary">
+              Termos de uso
+            </Link>
+            <Link
+              href="/politica-de-privacidade"
+              className="hover:text-primary"
+            >
+              Política de privacidade
+            </Link>
+            <Link href="/feedback" className="hover:text-primary">
+              Feedback
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  )
+}
