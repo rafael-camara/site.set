dc4f60b4a3a72bc0a011d7c3fcd59e7ed040aee9
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-07 13:37:04 -0300

    Fundamentos do Next.js - Criando o componente Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button'
+import { Header } from '@/components/header'
 
 export default function Home() {
   return (
     <div>
-      <h2>Home</h2>
-
-      <Button>Button</Button>
+      <Header />
     </div>
   )
 }
