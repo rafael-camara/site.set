668c0a904e1dcdbecc0be9bc0e32f133b093d498
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-08 11:32:46 -0300

    Fundamentos do Next.js - Ajustando as Fontes

--- src/components/support-section/support-section.tsx
+++ src/components/support-section/support-section.tsx
@@ -1,18 +1,10 @@
 import { HeartHandshake, PaintbrushVertical, Store } from 'lucide-react'
-import { PT_Sans_Caption } from 'next/font/google'
-
-const ptSansCaption = PT_Sans_Caption({
-  subsets: ['latin'],
-  weight: '700',
-})
 
 export const SupportSection = () => {
   return (
     <section className="pb-8 md:py-10 bg-gray-700">
       <div className="container flex flex-col items-center gap-12">
-        <h2
-          className={`${ptSansCaption.className} text-balance text-center text-heading-xl text-gray-100`}
-        >
+        <h2 className="font-sans text-balance text-center text-heading-xl text-gray-100">
           Sua loja de afiliados, simples, do jeito que deveria ser
         </h2>
 
@@ -33,7 +25,7 @@ export const SupportSection = () => {
 
           {/* Segundo Card */}
           <div className="flex flex-col text-left gap-2 rounded-lg p-6 md:p-12 bg-cyan-300">
-            <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-cyan-200 mb-4">
+            <div className="flex size-12 items-center justify-center rounded-lg bg-cyan-200 mb-4">
               <Store className="h-6 w-6 text-white" />
             </div>
             <strong className="text-heading-sm text-gray-100">
