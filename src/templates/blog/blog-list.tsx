767951fa5c4135e6b0312a75c10c11690cf22a49
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-09 16:24:29 -0300

    Fundamentos do Next.js - Criando o PostCard

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,5 +1,6 @@
 import { Search } from '@/components/search'
 import { useRouter } from 'next/router'
+import { PostCard } from './components/post-card'
 
 export function BlogList() {
   const router = useRouter()
@@ -29,6 +30,7 @@ export function BlogList() {
       </header>
 
       {/* Listagem de posts */}
+      <PostCard />
     </div>
   )
 }
