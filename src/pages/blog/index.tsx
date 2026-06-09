d4cdf065bfed178d2c786f569ed32accb1a5cae6
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-09 11:20:40 -0300

    Fundamentos do Next.js - Criando o componente de Search

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import { Search } from '@/components/search'
+
 export default function BlogPage() {
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -14,10 +16,11 @@ export default function BlogPage() {
               Dicas e estratégias para impulsionar seu negócio
             </h1>
           </div>
+
+          {/* Search */}
+          <Search />
         </div>
       </header>
-
-      {/* Search */}
 
       {/* Listagem de posts */}
     </div>
