080f88e0f139d97974abfda59e33a544b053c08c
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-09 17:12:50 -0300

    Fundamentos do Next.js - Listagem dinâmica de posts

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,4 +1,5 @@
 import { Search } from '@/components/search'
+import { allPosts } from 'contentlayer/generated'
 import { useRouter } from 'next/router'
 import { PostCard } from './components/post-card'
 import { PostGridCard } from './components/post-grid-card'
@@ -9,6 +10,8 @@ export function BlogList() {
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio'
+
+  const posts = allPosts
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -32,17 +35,20 @@ export function BlogList() {
 
       {/* Listagem de posts */}
       <PostGridCard>
-        <PostCard
-          title="Transformando seu negócio em uma loja virtual"
-          description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
-          date="20/12/2024"
-          slug="transformando"
-          image="/assets/primeiro-post.png"
-          author={{
-            avatar: '/customer-01.png',
-            name: 'Aspen Dokidis',
-          }}
-        />
+        {posts.map((post) => (
+          <PostCard
+            key={post._id}
+            title={post.title}
+            description={post.description}
+            date={new Date(post.date).toLocaleDateString('pt-BR')}
+            slug={post.slug}
+            image={post.image}
+            author={{
+              avatar: post.author.avatar,
+              name: post.author.name,
+            }}
+          />
+        ))}
       </PostGridCard>
     </div>
   )
