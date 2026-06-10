c7d8842a83b8564f4fb36841d1bb4fb133c7ee9d
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-10 17:40:26 -0300

    Fundamentos do Next.js - Aplicando SSG no blog

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,13 +1,5 @@
-// import { PostPage as Post } from '@/templates/blog';
-
-// export default function PostPage() {
-//   return <Post />;
-// }
+import { PostPage as Post } from '@/templates/blog'
 
 export default function PostPage() {
-  return (
-    <div>
-      <h2>Title</h2>
-    </div>
-  )
+  return <Post />
 }
--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,22 +1,26 @@
 import { Search } from '@/components/search'
-import { allPosts } from 'contentlayer/generated'
+import { Post } from 'contentlayer/generated'
 import { Inbox } from 'lucide-react'
 import { useRouter } from 'next/router'
 import { PostCard } from './components/post-card'
 import { PostGridCard } from './components/post-grid-card'
 
-export function BlogList() {
+export type BlogListProps = {
+  posts: Post[]
+}
+
+export function BlogList({ posts }: BlogListProps) {
   const router = useRouter()
   const query = router.query.q as string
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio'
 
-  const posts = query
-    ? allPosts.filter((post) =>
+  const postList = query
+    ? posts.filter((post) =>
         post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase()),
       )
-    : allPosts
+    : posts
 
   const hasPosts = posts?.length > 0
 
@@ -43,7 +47,7 @@ export function BlogList() {
       {/* Listagem de posts */}
       {hasPosts && (
         <PostGridCard>
-          {posts.map((post) => (
+          {postList.map((post) => (
             <PostCard
               key={post._id}
               title={post.title}
