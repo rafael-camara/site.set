e0b8f780e890082740a92d78e752eee3015199ac
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-12 10:55:31 -0300

    Fundamentos do Next.js - Migrando a Listagem de Posts

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,11 +1,13 @@
 import { cn } from '@/lib/utils'
 import { CircleX, SearchIcon } from 'lucide-react'
-import { useRouter } from 'next/router'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useCallback } from 'react'
 
 export const Search = () => {
   const router = useRouter()
-  const query = (router.query.q as string) ?? ''
+  const searchParams = useSearchParams()
+
+  const query = searchParams?.get('q') ?? ''
 
   const handleSearch = useCallback(
     (event: React.SubmitEvent<HTMLFormElement>) => {
@@ -19,14 +21,13 @@ export const Search = () => {
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newQuery = event.target.value
-    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
-      shallow: true,
+    router.push(`/blog?q=${encodeURIComponent(newQuery)}`, {
       scroll: false,
     })
   }
 
   const resetSearch = () => {
-    router.push('/blog', undefined, { shallow: true, scroll: false })
+    router.push('/blog', { scroll: false })
   }
 
   return (
