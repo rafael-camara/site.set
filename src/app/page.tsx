e904062e485b3eaf159b5f9e001b28d93d400a39
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-15 11:13:29 -0300

    Fundamentos do Next.js - Melhorando o SEO da Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import { LandingPage } from '@/templates/landing-page'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Site.Set',
+  description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: '/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+}
 
 export default function HomePage() {
   return <LandingPage />
