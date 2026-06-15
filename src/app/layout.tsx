95a57e54af14cd4a1a46be27a3097da48833f9db
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-15 18:01:53 -0300

    Fundamentos do Next.js - 05 - Melhorando o SEO da página do post

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Site.Set',
   description: 'Venda seus produtos como afiliado em um único lugar',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: '/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
