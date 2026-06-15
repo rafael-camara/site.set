e72eb8b0b1bcce74ab3729063b3f52767c7ab523
rafael-camara <rafaelcamara.377@gmail.com>
2026-06-15 11:32:44 -0300

    Fundamentos do Next.js -  Melhorando SEO na página de listagem de posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from '@/templates/blog'
 import { allPosts } from 'contentlayer/generated'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    url: '/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+}
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
